3e18dcfbf659c3de2c7b9a65bc6bf3388b4051c0
liuzhihao1 <liuzhihao1@yy.com>
2019-12-15 09:40:26 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+class MiniFormMik extends React.Component {
+  state = {
+    values: {
+      isGoing: true,
+      numberOfGuests: "1"
+    }
+  };
+
+  handleInputChange = event => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState(preState => ({
+      values: {
+        ...preState.values,
+        [name]: value
+      }
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        {this.props.children({
+          values: this.state.values,
+          handleInputChange: this.handleInputChange
+        })}
+      </>
+    );
+  }
+}
+
+class Reservation extends React.Component {
+  render() {
+    return (
+      <MiniFormMik>
+        {({ values, handleInputChange }) => {
+          console.log("val", values);
+
+          return (
+            <>
+              <form>
+                <label>
+                  Is going:
+                  <input
+                    name="isGoing"
+                    type="checkbox"
+                    checked={values.isGoing}
+                    onChange={handleInputChange}
+                  />
+                </label>
+                <br />
+                <label>
+                  Number of guests:
+                  <input
+                    name="numberOfGuests"
+                    type="number"
+                    value={values.numberOfGuests}
+                    onChange={handleInputChange}
+                  />
+                </label>
+              </form>
+
+              <pre>{JSON.stringify(values, null, 2)}</pre>
+            </>
+          );
+        }}
+      </MiniFormMik>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Reservation />
     </div>
   );
 }
